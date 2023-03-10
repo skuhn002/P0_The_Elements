890d68fc98dc9a1e8a01521238ceef0d5e0fc3e9
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-03-10 10:51:13 -0600

    extra styling 1.0

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function elementsUpdate(value) {
         console.log(jsonData);
     
         //update element info section
-        document.getElementById("element-title").innerHTML = await jsonData.name;
+        document.getElementById("element-title").innerHTML = await jsonData.name + ` (#${value})`;
         let elementImage = document.getElementById("element-image");
         elementImage.src = elementSpecifics[value].src;
         elementImage.alt = elementSpecifics[value].alt;
@@ -63,7 +63,14 @@ async function elementsUpdate(value) {
         document.getElementById("year-discovered-label").innerHTML = "Approximate Discovery Date: &nbsp";        
         document.getElementById("year-discovered").innerHTML = jsonData.yearDiscovered;
 
-        document.getElementById("ion-states-label").innerHTML = "The Ioniztion state of an element tells you what states of charge it normally accepts. An ionization state of -1 for example means the element has 1 more electron than it would need for the charge of it's protons to perfectly balance out the charge of it's electrons. Ionization states for this element are: ";
+        document.getElementById("ion-states-label").innerHTML = await `${jsonData.name} Ionization States: `;
         document.getElementById("ion-states").innerHTML = jsonData.oxidationStates
+        
+        let ionInfoP = document.createElement("P");
+        ionInfoP.innerHTML = "The Ioniztion state of an element tells you what states of charge it normally accepts. An ionization state of -1 for example means the element has 1 more electron than it would need for the charge of it's protons to perfectly balance out the charge of it's electrons.";
+
+        let ionInfoNode = document.getElementById("ion-extra-info");
+
+        ionInfoNode.append(ionInfoP);
     }
 }
