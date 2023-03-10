6110593f5d54d9e6e096e2f82cb02566664d5a30
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-03-09 20:30:08 -0600

    fix select listener

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let elementSelector = document.querySelectorAll('#select-element');
+let elementSelector = document.getElementById('select-element');
 
-function optionFunc(value) {
-    console.log(`My value is: ${value}`);
+async function optionFunc(value) {
+    console.log("ran");
+
+    if (value == "-1") {
+        console.log("no element selected");
+    } else {
+        console.log(`My value is: ${value}`);
+
+        let apiData = await fetch(`https://neelpatel05.pythonanywhere.com/element/atomicnumber?atomicnumber=${value}`);
+    
+        console.log("made it");
+    
+        let jsonData = apiData.json();
+    
+        await console.log(jsonData);
+    
+    }
 }
-
-elementSelector.addEventListener("change", optionFunc(elementSelector.value));
